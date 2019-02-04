2883dab8a10d1ee0e5f2abbfed705c1114db4404
Taylor Blanton <matthewtblanton@gmail.com>
2019-02-03 21:44:42 -0600

    updated to use functional setState from useState

--- src/use-counter.ts
+++ src/use-counter.ts
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 
 const useCounter = () : [number, () => void, () => void] => {
     const [count, setCounter] = useState(0);
-    const incrementCounter = () => { setCounter(count + 1); };
-    const decrementCounter = () => { setCounter(count - 1); };
+    const incrementCounter = () => { setCounter(prevCount => prevCount + 1); };
+    const decrementCounter = () => { setCounter(prevCount => prevCount - 1); };
 
     useEffect(() => {
         document.title = `Current counter is ${count}`;
